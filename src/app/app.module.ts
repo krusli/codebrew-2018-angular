4902aae52b302d31ba0b59159ba1dfac6630f67e
Kenneth <14309096+krusli@users.noreply.github.com>
2018-03-24 16:32:41 +1100

    Router works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { PageNotFoundComponent } from './not-found.component';
+import { SignupComponent } from './signup/signup.component';
 
+const appRoutes: Routes = [
+  // { path: '',   redirectTo: '/signup', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent }
+  { path: 'signup', component: SignupComponent },  // TODO
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    SignupComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+     appRoutes,
+     { enableTracing: true } // <-- debugging purposes only
+   )
   ],
   providers: [],
   bootstrap: [AppComponent]
