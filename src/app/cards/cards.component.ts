dbd7cbffa96c77b35346546502ec97a1c35ceec4
Kenneth <14309096+krusli@users.noreply.github.com>
2018-03-25 12:43:48 +1100

    Dynamic tags

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -11,33 +11,43 @@ export class CardsComponent implements OnInit {
       "name": "Volunteering at a rice farm",
       "location": "Mekong Delta, Vietnam",
       "description": "Help out local farmers during the harvest season.",
-      "photo": "farming.jpg"
+      "photo": "farming.jpg",
+      "tags": ["farming", "volunteer"],
+      "countrytags": ["vietnam"]
   },
   {
       "name": "Donation for tetanus vaccines",
       "location": "Laos",
       "description": "Donate to our vaccine program for school kids and safeguard their health.",
-      "photo": "vaccine.jpg"
+      "photo": "vaccine.jpg",
+      "tags": ["medical", "donation"],
+      "countrytags": ["laos"]
   },
   {
       "name": "Donate to the Global Fund for Education",
       "location": "Municipality of Dinalupihan, Philippines",
       "description": "The Global Education Fund is committed to helping children see possibilities beyond their experience every day. Make an impact in their lives.",
       "sponsor": "Global Education Fund",
-      "photo": "projDscpt.jpg"
+      "photo": "projDscpt.jpg",
+      "tags": ["education", "donation"],
+      "countrytags": ["philippines"]
   },
   {
       "name": "Funding for Medical Device at Buriram Hospital",
       "location": "Buriram, Thailand",
       "description": "Our patients are in need of access to better medical facilities. Please help us in the increasing demands on our Hospital.",
-      "photo": "CT.jpg"
+      "photo": "CT.jpg",
+      "tags": ["medical", "donation"],
+      "countrytags": ["thailand"]
   },
   {
       "name": "Donate to Sri Lanka Safe Water Project",
       "location": "Sri Lanka",
       "description": "Improve water condition for local communities by funding water filters.",
       "sponsor": "Foundation of Goodness",
-      "photo": "cleanWater.jpg"
+      "photo": "cleanWater.jpg",
+      "tags": ["health", "sanitation", "donation"],
+      "countrytags": ["sri lanka"]
   }];
 
 
